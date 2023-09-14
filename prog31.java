f4d6f9fd54101303ec9ac3e0d285baaab46ef6e4
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-14 11:47:29 +0530

    Create prog31.java

---
+++ prog31.java
@@ -0,0 +1,10 @@
+class Main{
+  public static void main(String args[]){
+    String first="Java";
+    String second="Python";
+    String third="Javascript";
+    System.out.println(first);
+    System.out.println(second);
+    System.out.println(third);
+  }
+}
