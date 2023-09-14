5a5af9ead6df38900467b9b27ca88a791092f202
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-14 12:42:46 +0530

    Create prog 33.java

---
+++ prog 33.java
@@ -0,0 +1,21 @@
+//concat & Substring
+class Main{
+  
+    public static void main(String args[]){
+        String first="Java";
+        System.out.println("First String: "+first);
+         String second="Programming";
+         System.out.println("Second String: "+second);
+         String JString=first.concat(second);
+          System.out.println("Joint String: "+JString);
+    }
+}
+class substring{
+    public static void main(String args[]){
+        String s1="Hello";
+        String s2="Bye";
+        String s3=s1.substring(2,4);
+        System.out.println(s3);
+    }
+    
+}
