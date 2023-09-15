eaa7b722dfee40f9a6ae37434ac2c70e8ba6a7a4
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:11:07 +0530

    Create Prog 2.java

---
+++ Prog 2.java
@@ -0,0 +1,24 @@
+// to check the number is even or odd
+import java.util.Scanner;
+
+class A {
+
+    public static void main(String[] args)
+
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+    System.out.println(" enter integer:-  ");
+
+    int n= sc.nextInt();
+
+    if(n%2==0)
+
+    System.out.println("the is " +n+ " is even");
+
+    else System.out.println( " the is " +n+ " is odd");
+
+    }
+
+}
