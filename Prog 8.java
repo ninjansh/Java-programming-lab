dd5b3d549a35fa9b0f69031b5d0a9266eed64ea4
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:19:48 +0530

    Create Prog 8.java

---
+++ Prog 8.java
@@ -0,0 +1,32 @@
+// factorial of given numbers
+import java.util.Scanner;
+
+public class main 
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter range: ");
+
+       int n = sc.nextInt();
+
+       int fact = 1;
+
+       for (int i=1;i<=n;i++)
+
+       {
+
+           fact=fact*i;
+
+       }
+
+        System.out.println(fact);
+
+    }
+
+}
