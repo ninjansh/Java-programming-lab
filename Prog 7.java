94608c73f3689d93f1f64f359066c72597af3846
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:18:54 +0530

    Create Prog 7.java

---
+++ Prog 7.java
@@ -0,0 +1,30 @@
+//WAP to print a table of a given number
+import java.util.Scanner;
+
+class A
+
+{
+
+    public static void main(String[] args)
+
+    {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println(" enter number :- ");
+
+        int n=sc.nextInt();
+
+        for(int i=1;i<=10;i++)
+
+        {
+
+            System.out.println(n+"*"+i+"="+n*i);
+
+            
+
+        }
+
+    }
+
+}
