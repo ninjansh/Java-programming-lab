ce680ad4bb36236db9db5b72b6e0c6a2f9dd6c73
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:09:49 +0530

    Create Prog 1.java

---
+++ Prog 1.java
@@ -0,0 +1,12 @@
+// to print an integer given by user
+import java.util.Scanner;
+class A {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+    System.out.println(" enter integer:-  ");
+    int n = sc.nextInt();
+    System.out.println(" num is " + n);
+
+    }
+}
