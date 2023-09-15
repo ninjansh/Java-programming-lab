aa11dd25f3853a7dc07deca8f47f37e496d452ef
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:16:41 +0530

    Create Prog 5.java

---
+++ Prog 5.java
@@ -0,0 +1,14 @@
+//WAP to print A to Z using loop
+import java.util.Scanner;
+class A {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        {
+            for(char i='A'; i<='Z'; i++)
+            {
+                System.out.print(i);
+            }
+        }
+    }
+}
