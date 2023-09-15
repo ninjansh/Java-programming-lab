048ec8ac2d6f28fbbe9608f2dd704b10548291f9
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-15 08:24:19 +0530

    Create Prog 10.java

---
+++ Prog 10.java
@@ -0,0 +1,75 @@
+// WAP in java to demonstrate usage of Primitive data types – Boolean,Char,Byte,Short,Int,Long,Float and Double
+import java.util.Scanner;
+
+
+
+public class PrimitiveDataTypesDemo {
+
+public static void main(String[] args) {
+
+// Boolean
+
+boolean isJavaFun = true;
+
+System.out.println("Is Java fun? " + isJavaFun);
+
+
+
+// Char
+
+char grade = 'A';
+
+System.out.println("My grade is: " + grade);
+
+
+
+// Byte
+
+byte age = 25;
+
+System.out.println("My age is: " + age);
+
+
+
+// Short
+
+short numberOfStudents = 1000;
+
+System.out.println("Number of students: " + numberOfStudents);
+
+
+
+// Int
+
+int population = 1000000;
+
+System.out.println("Population: " + population);
+
+
+
+// Long
+
+long worldPopulation = 7800000000L; // Note the 'L' suffix for long literals
+
+System.out.println("World population: " + worldPopulation);
+
+
+
+// Float
+
+float pi = 3.14159f; // Note the 'f' suffix for float literals
+
+System.out.println("Value of pi: " + pi);
+
+
+
+// Double
+
+double e = 2.71828;
+
+System.out.println("Value of Euler's number: " + e);
+
+}
+
+}
+
