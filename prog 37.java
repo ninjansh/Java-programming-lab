02ac0d266106235fe95841e9e545e9272473c25a
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-26 12:06:50 +0530

    Create prog 37.java

---
+++ prog 37.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class array
+{
+	public static void main(String[] args) 
+	{
+	int[]age={new int[]{1,2,3};
+	System.out.println(age.length);
+	for (int a:age)
+	{
+	    System.out.println(a);
+	}
+	    
+	}
+    
+}
+// save as 'array.java'
