dcdfc971ed3dc592192bbd671d483a4754d07213
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-26 12:36:45 +0530

    Create prog 39.java

---
+++ prog 39.java
@@ -0,0 +1,14 @@
+// array sorting
+import java.util.Arrays;
+ public class a{
+     public static void main (String[]args){
+         int a[]={2000,1998,1990,2002};
+         String[]s={"Joy","zoom","Shyam"};
+         System.out.println("Integer array is: "+ Arrays.toString(a));
+         Arrays.sort(a);
+          System.out.println("sorting array is: "+ Arrays.toString(a));
+           System.out.println("String array is: "+ Arrays.toString(a));
+           Arrays.sort(s);
+     }
+ }
+//save as 'a.java'
