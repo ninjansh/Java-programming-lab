254d1e41b4595ab3aa4d527317ec9b563594cc10
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-09-26 12:43:14 +0530

    Create prog 40.java

---
+++ prog 40.java
@@ -0,0 +1,13 @@
+// Wap to max and min in array
+import java.util.Arrays;
+ public class a{
+     public static void main (String[]args){
+         int a[]={2000,1998,1990,2002};
+         String[]s={"Joy","zoom","Shyam"};
+         System.out.println("Integer array is: "+ Arrays.toString(a));
+         Arrays.sort(a);
+          System.out.println("Minimun is: "+ a[0]);
+           System.out.println("maximum is: "+ a[3]);
+     }
+ }
+// save as 'a.java'
