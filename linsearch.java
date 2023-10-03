84d7e25eca12f61556981f56adee53d567cf80f4
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-10-03 11:53:59 +0530

    Create linsearch.java

---
+++ linsearch.java
@@ -0,0 +1,16 @@
+// linear search in java array
+public class linsearch{
+    public static int linearSearch(int[]arr,int key){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
+}
+public static void main(String args[]){
+    int[]a1={10,20,30,40,50,60};
+    int key=50;
+    System.out.println(key+"is found at index: "+linearSearch(a1,key));
+}
+}
