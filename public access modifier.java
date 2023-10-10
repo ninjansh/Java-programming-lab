7f960e5e9f073a2f54fe86a4dd2cc8ba01ae5b4d
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-10-10 12:18:35 +0530

    Create public access modifier.java

---
+++ public access modifier.java
@@ -0,0 +1,16 @@
+// demonstration of public access modifier
+class Animal{
+    public int legCount;
+    public void display(){
+        System.out.println("I am an animal");
+        System.out.println("I have"+ legCount+"legs.");
+    }
+}
+public class Main{
+    public static void main(String args[]){
+        Animal animal=new Animal();
+        animal.legCount=4;
+        animal.display();
+    }
+}
+// save as "Main.java"
