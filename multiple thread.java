941c79c011fd31320ee016bdae8538e8c7fc0ef3
ninjansh <142496574+ninjansh@users.noreply.github.com>
2023-10-31 12:11:47 +0530

    Create multiple thread.java

---
+++ multiple thread.java
@@ -0,0 +1,14 @@
+// multiple thread 
+public class Main extends Thread{
+    public void run(){
+        System.out.println("Welcome to CSIT");
+    }
+     public static void main(String args[]){
+         Main t1=new Main();
+          Main t2=new Main();
+           Main t3=new Main();
+           t1.start();
+            t2.start();
+             t3.start();
+     }
+}
